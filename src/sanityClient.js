85f333ccb54c67af5435fb3d2c79a7207c05ea0a
Dheerendarkumar11 <dhrajch@gmail.com>
2024-11-03 22:53:14 +0530

    token add

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -5,7 +5,8 @@ const client = createClient({
   projectId: 'l0ay2hgi',    
   dataset: 'production',         
   apiVersion: '2024-10-23',      
-  useCdn: true,                  
+  useCdn: true,   
+  token: "sko4cHoiMG1tA28Yy1V9QtIL9BBWNKIOwmrK6PnOiLleQ7r8Hjb3cdK10DRLni1kd9sDZvg4p4bK2KEI3jodbYpjDSN4nYpgnCMJwsNO4L18inL539QLQGxcD1d742JncCmciBwohJbFmhwrGfJdKaG6EZ2peIGxaoycBkjBDYxu3x2DAA9v"               
 });
 
 export default client;
