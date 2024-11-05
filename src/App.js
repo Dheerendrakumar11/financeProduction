83ba6d61267f26b7f8e4a126cdc73f27bc9fc975
Dheerendarkumar11 <dhrajch@gmail.com>
2024-11-06 01:54:36 +0530

    change name page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
 import PostContent from './PostContent';
-import CategoryPost from './CategoryPosts';
+import CategoryPost from './categoryPosts';
 
 export default function App() {
   return (
